ac3df5339661acfd92bd3bfb1186f6b80bdb2a1f
Liam-Cafearo <lcafearo@gmail.com>
2017-08-29 17:56:59 +0100

    Updated folder structure and added new lessons

---
+++ course-work/Stream1/jQuery/The_Importance_of_this/Hiding_Paragraphs/js/script.js
@@ -0,0 +1,7 @@
+/* This specifies the exact element (object) that’s targeted. 
+When working with the Document Object Model (DOM), this refers to the selected or active node. 
+jQuery will assign the active element to the “this” keyword. */
+
+$('p').click(function() {
+	$(this).slideToggle('slow'); 
+});
