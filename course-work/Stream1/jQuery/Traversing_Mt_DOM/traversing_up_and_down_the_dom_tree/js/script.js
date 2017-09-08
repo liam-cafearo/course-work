63c69305d83ce4095cc326dbab268ddd76f9568f
liam-cafearo <lcafearo@gmail.com>
2017-09-08 07:23:14 +0100

    ammended lesson files

--- course-work/Stream1/jQuery/Traversing_Mt_DOM/traversing_up_and_down_the_dom_tree/js/script.js
+++ course-work/Stream1/jQuery/Traversing_Mt_DOM/traversing_up_and_down_the_dom_tree/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	// Here we are saying "when we click on a "p" tag"
 	$("p").click(function() {
-		// Set the children elements (in our case the "a" tag) within the selected p tag (this) to yellow.
+		// retun all the children elements (in our case the "a" tag) within the selected p tag (this) as yellow.
 		$(this).children("a").css("background-color", "yellow");
 	})
 }); 
