a2942e0b020c261c3e62ee15d34d1f2ae72fc229
liam-cafearo <lcafearo@gmail.com>
2017-09-13 15:43:33 +0100

    added finished traversing sideways challenge files

--- course-work/Stream1/jQuery/Traversing_Mt_DOM/traversing_sideways/js/script.js
+++ course-work/Stream1/jQuery/Traversing_Mt_DOM/traversing_sideways/js/script.js
@@ -8,4 +8,16 @@ $(document).ready(function() {
 	$(".card").click(function() {
 		$(this).toggleClass("highlight");
 	});
+	// Selecting the id we added to the HTML, when clicked on
+	$("#select_btn").click(function(){
+		// .card:not(.highlight) == Select all .card classes except those with class="highlight"
+		// .hide == hides the the ones not selected.
+		$(".card:not(.highlight)").hide();
+	});
+	// all cards will selected/re-appear. #all_btn is the id we added to the HTML
+	$("#all_btn").click(function(){
+		// show all elements with a class of .card
+		// we don't use this because we are selecting a different part of CSS
+		$(".card").show();
+	});
 });
