6feb64a2d10ba501ccd7e846e988366c403f7a1b
liam-cafearo <lcafearo@gmail.com>
2017-09-17 17:37:58 +0100

    Added challenge b & c files

--- course-work/Stream1/jQuery/Traversing_Mt_DOM/other_traversing_methods/challenge_b/js/my_script.js
+++ course-work/Stream1/jQuery/Traversing_Mt_DOM/other_traversing_methods/challenge_b/js/my_script.js
@@ -1,4 +1,12 @@
-// will assign css class .selection to the odd numbered table rows
+// will wait till page ready to run script.
 $(document).ready(function() {
-	$("tr:odd").addClass("selection");
+	// When a table header is clicked
+	$("th").click(function(){
+		// this statement removes the "selection" class 
+		// from all children (in this case the <th> tag) of the <tr> tag.
+		$("tr").children().removeClass("selection");
+		// This statement adds the class "selection" 
+		// to (this) selected table header and its siblings (<td>) row only.
+		$(this).siblings().addClass("selection");
+	});
 });
