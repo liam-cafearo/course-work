5654a54a6a49ab1f6d5931a71ee761cbe9d24a30
Liam-Cafearo <lcafearo@gmail.com>
2017-11-24 00:01:29 +0000

    Updated JS Testing with Jasmine files

--- course-work/Stream1/javascript_tdd/spec/calcSpec.js
+++ course-work/Stream1/javascript_tdd/spec/calcSpec.js
@@ -10,7 +10,7 @@
 
 // modify to actually check that the function can take numbers as input and add them
 
-describe("Calculator", function() {
+/*describe("Calculator", function() {
 	describe('Addition function', function() {
 		it('should add two numbers together and return the result', function() {
 			expect(addition(2, 2)).toBe(4);
@@ -24,12 +24,169 @@ describe("Calculator", function() {
 		});
 
 		// Defensive programing
+		// add a spy to check if a function has been invoked.
+
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			addition("Hello";) // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguements most both be a number");
+		});
+	});
+});*/
+
+// update the test to reflect the changes for our object in calc.js
+
+/*describe("Calculator", function() {
+
+	var calc = new Calculator();
+
+	describe('Addition function', function() {
+		it('should add two numbers together and return the result', function() {
+			calc.add(2);
+			calc.add(2)
+			expect(calc.value).toBe(4);
+		});
+
+		// To make sure that the function doesn’t just always return 4, 
+		// let’s now add another test to check for different numbers
+
+		it("should not return 4 if the parameters given don't add up to 4", function() {
+			calc.add(12);
+			calc.add(7);
+			expect(calc.value).toBe(19);
+		});
+
+		// Defensive programing
+		// add a spy to check if a function has been invoked.
+
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			calc.add(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
+		});
+	});
+});*/
+
+// update to add beforeEach method so that the value property is reset to 0
+// each time a test is run.
+
+/*describe("Calculator", function() {
+
+	//  Pass the beforeEach method a valid JS value such as a function.
+	// tis resets the value to 0 before each test.
+	beforeEach(function() {
+		calc = new Calculator();
+	});
+	describe('Addition function', function() {
+		it('should add two numbers together and return the result', function() {
+			calc.add(2);
+			calc.add(2)
+			expect(calc.value).toBe(4);
+		});
+
+		// To make sure that the function doesn’t just always return 4, 
+		// let’s now add another test to check for different numbers
+
+		it("should not return 4 if the parameters given don't add up to 4", function() {
+			calc.add(12);
+			calc.add(7);
+			expect(calc.value).toBe(19);
+		});
+
+		// Defensive programing
 		// add a spy and tpye of to check that the function receives a number
 
 		it("should have called the alert function if either number is undefined", function() {
 			spyOn(window, "alert");
-			addition("Hello";)
-			expect(window.alert).toHaveBeenCalledWith("Arguements most both be a number");
+			calc.add(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
+		});
+	});
+});*/
+
+// Add suntract, multiply and divide tests
+
+describe("Calculator", function() {
+
+	//  Pass the beforeEach method a valid JS value such as a function.
+	//  this resets the value to 0 before each test
+	beforeEach(function() {
+		calc = new Calculator();
+	});
+	describe('Addition function', function() {
+		it('should add two numbers together and return the result', function() {
+			calc.add(2);
+			calc.add(2);
+			expect(calc.value).toBe(4);
+		});
+
+		// To make sure that the function doesn’t just always return 4, 
+		// let’s now add another test to check for different numbers
+
+		it("should not return 4 if the parameters given don't add up to 4", function() {
+			calc.add(12);
+			calc.add(7);
+			expect(calc.value).toBe(19);
+		});
+
+		// Defensive programing
+		// add a spy to check if a function has been invoked.
+
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			calc.add(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
+		});
+	});
+	describe('Subtraction function', function() {
+		it('should subtract two numbers together and return the result', function() {
+			calc.add(5); // add a number so that the starting value is no longer 0
+			calc.subtract(2);
+			expect(calc.value).toBe(3);
+		});
+		it("should not return 3 if the parameters given don't subtract to 3", function() {
+			calc.add(12); // add a number so that the starting value is no longer 0
+			calc.subtract(7);
+			expect(calc.value).toBe(5);
+		});
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			calc.subtract(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
+		});
+	});
+	describe('Multiply function', function() {
+		it('should mulitply two numbers together and return the result', function() {
+			calc.add(2); // add a number so that the starting value is no longer 0
+			calc.multiply(2);
+			expect(calc.value).toBe(4);
+		});
+		it("should not return 4 if the parameters given don't multiply to 4", function() {
+			calc.add(2) // add a number so that the starting value is no longer 0
+			calc.multiply(12); 
+			expect(calc.value).toBe(24);
+		});
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			calc.multiply(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
+		});
+	});
+	describe('divide function', function() {
+		it('should divide two numbers together and return the result', function() {
+			calc.add(10); // add a number so that the starting value is no longer 0
+			calc.divide(2);
+			expect(calc.value).toBe(5);
+		});
+		it("should not return 5 if the parameters given don't divide to 5", function() {
+			calc.add(20) // add a number so that the starting value is no longer 0
+			calc.divide(2); 
+			expect(calc.value).toBe(10);
+		});
+		it("should have called the alert function if either number is undefined", function() {
+			spyOn(window, "alert");
+			calc.divide(); // pass a parameter that will fail the test.
+			expect(window.alert).toHaveBeenCalledWith("Arguement must be a number");
 		});
 	});
 });
--- course-work/Stream1/javascript_tdd/src/calc.js
+++ course-work/Stream1/javascript_tdd/src/calc.js
@@ -21,11 +21,57 @@
 
 // Ammend addition function for defensive programming and spyOn
 
-function addition(firstNumber, secondNumber) {
+/*function addition(firstNumber, secondNumber) {
 	if (typeof(firstNumber) == "number" && typeof(secondNumber) == "number") {
 		var sum = firstNumber +secondNumber;
 		return sum;
 	} else {
 		alert("Arguements must both be a number");
 	}
-}
+}*/
+
+// Create the calculator as an object this means all the logic necessary 
+// for the calculator is safely contained within one place rather 
+// than creating separate functions
+
+Calculator = function() {
+	this.value = 0;
+};
+
+Calculator.prototype.add = function(number) {
+	if (typeof(number) == "number") {
+		this.value += number;
+	} else {
+		alert("Arguement must be a number");
+	}
+};
+
+// add subtract function
+
+Calculator.prototype.subtract = function(number) {
+	if (typeof(number) == "number") {
+		this.value -= number;
+	} else {
+		alert("Arguement must be a number");
+	}
+};
+
+// add multiply function
+
+Calculator.prototype.multiply = function(number) {
+	if (typeof(number) == "number") {
+		this.value *= number;
+	} else {
+		alert("Arguement must be a number");
+	}
+};
+
+// add divide function
+
+Calculator.prototype.divide = function(number) {
+	if(typeof(number) == "number") {
+		this.value /= number;
+	} else {
+		alert("Arguement must be a number");
+	}
+};
